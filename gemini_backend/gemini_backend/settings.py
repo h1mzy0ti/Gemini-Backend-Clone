e536e1e7ca3d8fab1a35db28d6281bc82d1b69f8
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-07-19 13:07:44 +0530

    Configure PostgreSQL, Celery and Redis

--- gemini_backend/gemini_backend/settings.py
+++ gemini_backend/gemini_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt'
 ]
+ADDED_APPS = [
+    'user_auth',
+    'chatroom',
+    'gemini_api',
+    'subscriptions'
+]
+INSTALLED_APPS += ADDED_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -74,11 +83,19 @@ WSGI_APPLICATION = 'gemini_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD' : config("DB_PASSWORD"),
+        'HOST' : config('DB_HOST'),
+        'PORT' : config('DB_PORT'),
     }
 }
 
+# Celery 
+CELERY_BROKER_URL = 'redis//localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_TIMEZONE = 'Asia/Kolkata'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
