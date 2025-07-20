5d32c3618cbe42d19676cc9dc6284b8a643d1ae0
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-07-20 16:04:52 +0530

    Fix celery.py

--- gemini_backend/gemini_backend/celery.py
+++ gemini_backend/gemini_backend/celery.py
@@ -1,5 +1,5 @@
 import os
-
+import ssl
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
@@ -9,5 +9,12 @@ app = Celery('gemini_backend')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+# Add SSL settings for Redis
+app.conf.broker_use_ssl = {
+    'ssl_cert_reqs': ssl.CERT_NONE
+}
+app.conf.redis_backend_use_ssl = {
+    'ssl_cert_reqs': ssl.CERT_NONE
+}
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
